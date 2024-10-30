3d1a0364c73133d71cf828c4a963688e39a0da5f
loukajc <loukajc@gmail.com>
2024-10-30 16:14:39 +0100

    safer install script

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ IFS=$'\n\t'
 export PATH="$PATH:$HOME/.local/bin"
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
+if [[ ! $(grep -F 24.04 /etc/issue.net -c) -eq 1 ]] ; then 
+    echo "Lalubuntu latest has been tested on Ubuntu 24.04 only."
+    echo "Please install Ubuntu 24.04 or attempt install at your own risks"
+    exit 42
+fi
+
 echo "[*] Installing LaluBuntu. Go grab yourself a coffee, this will run for A WHILE. :D"
 ansible-playbook -vvv -i inventory.ini --ask-become main.yml
 echo "[*] Done! Logout, then you MUST pick Regolith/X11 (bottom right of sessions screen) and Login!"
